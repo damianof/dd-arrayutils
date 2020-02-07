91455f8ad28ea49e6a6ce09aa23df8df18f1a400
Damiano <damiano@iMacRetina.local>
2020-02-07 07:24:09 -0500

    unit tests updates

--- tests/arrays/ArrayUtils.orderby.spec.ts
+++ tests/arrays/ArrayUtils.orderby.spec.ts
@@ -6,7 +6,26 @@ describe('ArrayUtils', () => {
 
 	describe('orderBy', () => {
 
-		it('should sort item array correctly by name', () => {
+		it('should sort item array correctly by name when two items have the same name', () => {
+			
+			const items: TestItem[] = [{
+				id: 1,
+				selected: false,
+				name: 'A Item'
+			}, {
+				id: 2,
+				selected: false,
+				name: 'A Item'
+			}]
+			arrayUtils.orderBy(items, 'name')
+			console.info('items: orderby name', items)
+
+			expect(items[0].id).to.equal(1)
+			expect(items[1].id).to.equal(2)
+
+		})
+
+		it('should sort items with different names correctly by name', () => {
 			
 			const items: TestItem[] = testItemHelper.createMockedItems(3)
 			arrayUtils.orderBy(items, 'name')
@@ -18,7 +37,7 @@ describe('ArrayUtils', () => {
 
 		})
 
-		it('should sort item array correctly by id', () => {
+		it('should sort items with different ids correctly by id', () => {
 			
 			const items: TestItem[] = testItemHelper.createMockedItems(3)
 			arrayUtils.orderBy(items, 'id')
